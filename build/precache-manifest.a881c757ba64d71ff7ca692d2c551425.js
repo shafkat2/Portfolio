027637d4450831e20e8fe1cc8f883b776ccf0da4
shafkat2 <shafkat.waheed@northsouth.edu>
2019-03-19 03:23:51 +0600

    My Resume

---
+++ build/precache-manifest.a881c757ba64d71ff7ca692d2c551425.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07d518eeff7e318bf0c1",
+    "url": "/static/js/main.ab6c9b35.chunk.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "07d518eeff7e318bf0c1",
+    "url": "/static/css/main.7420791d.chunk.css"
+  },
+  {
+    "revision": "c8cee65079c56595857094ceb4987883",
+    "url": "/index.html"
+  }
+];
